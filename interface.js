a286ba4befdd990e52174c4fdbbcbe25e07fead0
Phil Smithies <philsmithies@gmail.com>
2021-05-12 17:02:02 +0200

    pomodoro timer refactored

--- interface.js
+++ interface.js
@@ -1,13 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
   const noteList = new NoteList();
   const notesAvailable = noteList.showNotes();
+  // const notes = new Note("hello", "body");
+  // const notes2 = new Note("hello2", "body2");
+  // noteList.storeNote(notes);
+  // noteList.storeNote(notes2);
 
   document.getElementById("submitButton").addEventListener("click", function() {
-    const title = document.getElementById("titleBox").value;
-    const body = document.getElementById("bodyBox").value;
+    let title = document.getElementById("titleBox").value;
+    let body = document.getElementById("bodyBox").value;
     const note = new Note(title, body);
     noteList.storeNote(note);
+    document.getElementById("titleBox").value = '';
+    document.getElementById("bodyBox").value = '';
   });
+
+  document.getElementById("showNotes").addEventListener('click', function() {
+    console.log('clicked once')
+
+  function myFunction(note) {
+    console.log('clicked')
+    let noteTitle = document.createElement('h3')
+    let noteBody = document.createElement('p')
+    let div = document.createElement('div');
+    noteTitle.textContent = note.title
+    noteBody.textContent = note.body
+    div.appendChild(noteTitle)
+    div.appendChild(noteBody)
+    div.style['backgroundColor'] = 'yellow'
+
+    const showNotesContainer = document.getElementById('showNotesContainer')
+    showNotesContainer.appendChild(div)
+  }
+  console.log(notesAvailable)
+  notesAvailable.forEach(myFunction)
+  
+  })
 
 
 
