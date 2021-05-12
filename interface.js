1e8c3fd4871b68c0c5f1c5da53dcec24c256761d
Dominic Meddick <dominicmeddick@gmail.com>
2021-05-12 14:53:38 +0100

    first event listener working

--- interface.js
+++ interface.js
@@ -1,46 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let noteList = new Notelist();
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  const noteList = new NoteList();
+  const notesAvailable = noteList.showNotes();
 
-//   const thermostat = new Thermostat();
-//   updateTemperature();
+  document.getElementById("submitButton").addEventListener("click", function() {
+    const title = document.getElementById("titleBox").value;
+    const body = document.getElementById("bodyBox").value;
+    const note = new Note(title, body);
+    noteList.storeNote(note);
+  });
 
-//   document.querySelector('#temperature-up').addEventListener('click', () => {
-//     thermostat.up();
-//     updateTemperature();
-//   });
 
-//   document.querySelector('#temperature-down').addEventListener('click', () => {
-//     thermostat.down();
-//     updateTemperature();
-//   });
 
-//   document.querySelector('#temperature-reset').addEventListener('click', () => {
-//     thermostat.resetTemperature();
-//     updateTemperature();
-//   });
-
-//   document.querySelector('#powersaving-on').addEventListener('click', () => {
-//     thermostat.switchPowerSavingModeOn();
-//     document.querySelector('#power-saving-status').innerText = 'on';
-//     updateTemperature();
-//   })
-
-//   document.querySelector('#powersaving-off').addEventListener('click', () => {
-//     thermostat.switchPowerSavingModeOff();
-//     document.querySelector('#power-saving-status').innerText = 'off';
-//     updateTemperature();
-//   })
-// });
-
-// document.getElementById("getCity").addEventListener("click", () => {
-//   const citySearch = document.getElementById("getCityText").value;
-//   document.getElementById("getCityText").value = '';
-
-document.getElementById("submitButton").addEventListener("click", () => {
-  const title = document.getElementById("titleBox").value;
-  const body = document.getElementById("bodyBox").value;
-  let note = new Note();
-  Note.notes 
-  
 });
